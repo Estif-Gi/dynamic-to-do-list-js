3a4c14f6044fa74973ae2b9fc16839d50f403e90
Estifanos Girma <estifanosgirma4@gmail.com>
2025-01-26 12:26:06 +0300

    to do

---
+++ script.js
@@ -0,0 +1,36 @@
+const addButton = document.getElementById('add-task-btn');
+const taskInput = document.getElementById('task-input');
+const taskList = document.getElementById('task-list');
+function addTask() {
+    let taskText = taskInput.value.trim();
+    if(taskText == "")
+        {
+            alert('please inter a task');
+        }
+        else
+        {
+            const li = document.createElement('li');
+            li.textContent = taskText;
+            const button = document.createElement('button');
+            button.textContent = "Remove";
+            button.setAttribute('class','remove-btn');
+            button.addEventListener('click' , ()=>{
+                li.style.display = 'none'
+            });
+            li.appendChild(button);
+            taskList.appendChild(li); 
+            taskInput.value = '';
+        }
+    } 
+    addButton.addEventListener('click', () => {
+        addTask()
+    });
+    taskInput.addEventListener('keypress',()=>{
+        if(event.key === 'Enter'){
+            addTask();
+        };
+    });
+    document.addEventListener('DOMContentLoaded', addTask);
+
+
+// addTask()
